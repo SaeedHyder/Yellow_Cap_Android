3f55b811a8fd93d15891405eeb2d4dc87fd17631
shahsyed <shahsyed@axact.com>
2017-05-24 08:48:09 -0700

    Signed-off-by: shahsyed <shahsyed@axact.com>

---
+++ app/src/main/java/com/app/yellowcap/fragments/UserJobsFragment.java
@@ -0,0 +1,15 @@
+package com.app.yellowcap.fragments;
+
+import com.app.yellowcap.R;
+import com.app.yellowcap.fragments.abstracts.BaseFragment;
+
+/**
+ * Created on 5/24/2017.
+ */
+
+public class UserJobsFragment extends BaseFragment {
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_user_jobs;
+    }
+}
