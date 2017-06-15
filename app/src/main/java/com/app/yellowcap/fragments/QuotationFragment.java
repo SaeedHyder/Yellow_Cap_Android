c25b9e632c4421fc693359df400304cf3888d612
shahsyed <shahsyed@axact.com>
2017-06-15 06:33:32 -0700

    Signed-off-by: shahsyed <shahsyed@axact.com>

--- app/src/main/java/com/app/yellowcap/fragments/QuotationFragment.java
+++ app/src/main/java/com/app/yellowcap/fragments/QuotationFragment.java
@@ -179,6 +179,7 @@ public class QuotationFragment extends BaseFragment implements View.OnClickListe
                     acceptQuotation();
                 }
                 break;
+            
 
         }
 
