60d33f18cfc09068f4804581d93ba1f8969e265e
saeedhyder <saeedhyder@axact.com>
2018-03-01 19:15:22 +0500

    Signed-off-by: saeedhyder <saeedhyder@axact.com>

--- app/src/main/java/com/ingic/yellowcap/global/WebServiceConstants.java
+++ app/src/main/java/com/ingic/yellowcap/global/WebServiceConstants.java
@@ -1,5 +1,6 @@
 package com.ingic.yellowcap.global;
 
 public class WebServiceConstants {
-    public static final String SERVICE_URL = "https://www.stagingic.com/portfolio/yellow-cap/api/";
+  //  public static final String SERVICE_URL = "https://www.stagingic.com/portfolio/yellow-cap/api/";
+    public static final String SERVICE_URL = "http://yellowcap.ae/admin/api/";
 }
